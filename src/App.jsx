884ff898d349cae3202044835a3680b50df470d2
Raihan Chetta Ananta Ariano <chettaananta@gmail.com>
2026-02-02 22:50:02 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
+
+function App() {
+  const [theme, setTheme] = useState("light");
+  return (
+    <div className="dark:bg-black relative">
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ThemeToogleBtn.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import assets from "../assets/assets";
+
+const ThemeToogleBtn = ({ theme, setTheme }) => {
+  return (
+    <>
+      <button>
+        {theme === "dark" ? (
+          <img
+            onClick={() => setTheme("light")}
+            src={assets.sun_icon}
+            className="size-8.5 p-1.5 border border-gray-500 rounded-full"
+          />
+        ) : (
+          <img
+            onClick={() => setTheme("dark")}
+            src={assets.moon_icon}
+            className="size-8.5 p-1.5 border border-gray-500 rounded-full"
+            alt=""
+          />
+        )}
+      </button>
+    </>
+  );
+};
+
+export default ThemeToogleBtn;
